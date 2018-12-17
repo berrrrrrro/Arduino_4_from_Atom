457322102acb4e7b1b3f6e506cc45365b20ddbdb
Ibrahim Shikdaher <ibrahim.shikdaher@gmail.com>
2018-12-17 05:14:12 +0100

    quatro

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(900);                       // wait for a second
+  delay(90000000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100000);   
+  delay(100000);
 }
